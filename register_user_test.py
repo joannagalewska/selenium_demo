5109099585efef1e3f0b9fee57e69993f0bc3cba
Joanna Galewska <4431886+joannagalewska@users.noreply.github.com>
2023-05-21 23:41:31 +0200

    Update register_user_test.py

--- register_user_test.py
+++ register_user_test.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.common.by import By
 
-
 def test_register_user():
-    driver = webdriver.Firefox()
+    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
     driver.maximize_window()
     driver.implicitly_wait(30)
     driver.get("http://seleniumdemo.com/")
