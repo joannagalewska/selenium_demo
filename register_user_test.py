3a52f2266487a15198b31f037cc13ab52861875a
Joanna Galewska <4431886+joannagalewska@users.noreply.github.com>
2023-05-22 00:27:46 +0200

    Update register_user_test.py

--- register_user_test.py
+++ register_user_test.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 def test_register_user():
-    driver = webdriver.Firefox()  
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
     driver.implicitly_wait(30)
     driver.get("http://seleniumdemo.com/")
     driver.find_element(By.XPATH, "//span[text()='My account']").click()
