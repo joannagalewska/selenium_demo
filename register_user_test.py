51934cb804c9ae0fdcc14405b6ae8f3a78ff4f54
Joanna Galewska <4431886+joannagalewska@users.noreply.github.com>
2023-05-21 23:40:17 +0200

    Update register_user_test.py

--- register_user_test.py
+++ register_user_test.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
 def test_register_user():
-    DRIVER="geckodriver"
-    service = Service(executable_path=DRIVER)
-    driver = webdriver.Firefox(service=service)
+    driver = webdriver.Firefox()
     driver.maximize_window()
     driver.implicitly_wait(30)
     driver.get("http://seleniumdemo.com/")
@@ -16,4 +13,3 @@ def test_register_user():
     driver.find_element(By.NAME, "register").click()
     error = driver.find_element(By.XPATH, "//ul[@class='woocommerce-error']//li").text
     assert "An account is already registered with your email address" in error
-    driver.quit()
