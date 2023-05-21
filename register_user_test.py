7509baca6eb3cb4b56d8dcbd2807cb60fae209bb
Joanna Galewska <4431886+joannagalewska@users.noreply.github.com>
2023-05-21 21:49:20 +0200

    Add files via upload

---
+++ register_user_test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_register_user():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.implicitly_wait(30)
+    driver.get("http://seleniumdemo.com/")
+    driver.find_element(By.XPATH, "//span[text()='My account']").click()
+    driver.find_element(By.ID, "reg_email").send_keys("test@test.pl")
+    driver.find_element(By.ID, "reg_password").send_keys("test@test.pl")
+    driver.find_element(By.NAME, "register").click()
+    error = driver.find_element(By.XPATH, "//ul[@class='woocommerce-error']//li").text
+    assert "An account is already registered with your email address" in error
