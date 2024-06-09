1a49b754f1846a7175626aa69182679ec66af6c5
Joanna Galewska <4431886+joannagalewska@users.noreply.github.com>
2024-06-09 07:50:03 +0200

    Update register_user_test.py

--- register_user_test.py
+++ register_user_test.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 def test_register_user():
-    driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get("http://seleniumdemo.com/")
     driver.find_element(By.XPATH, "//span[text()='My account']").click()
     driver.find_element(By.ID, "reg_email").send_keys("test@test.pl")
