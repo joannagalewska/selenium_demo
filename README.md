857233441cca663a22281f60ae10ae0b587089d9
joannagalewska <galewska.joanna@gmail.com>
2025-06-13 21:49:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# selenium_demo
+
+To install requirements: 
+```
+pip install -r requirements.txt
+```
+
+To run application:
+```
+pytest
+```
+To run report:
+```
+pytest --alluredir=reports
+```
---
+++ test_register_user.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
+
+
+def test_register_user():
+    service = ChromeService(executable_path="/opt/homebrew/bin/chromedriver")
+    driver = webdriver.Chrome(service=service)
+
+    random_number = random.randint(1000, 9999)
+    unique_email = f"test{random_number}@test.pl"
+
+    driver.get("http://seleniumdemo.com/")
+    driver.find_element(By.XPATH, "//span[text()='My account']").click()
+    driver.find_element(By.ID, "reg_email").send_keys(unique_email)
+    driver.find_element(By.ID, "reg_password").send_keys("test@test.pl")
+    driver.find_element(By.NAME, "register").click()
+
+    wait = WebDriverWait(driver, 10)
+    entry_title = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "entry-title")))
+    assert "My account" in entry_title.text
+
+    driver.quit()
