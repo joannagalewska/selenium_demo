fab3f973e8a2752ca227519409c98bbe8e670a5e
joannagalewska <galewska.joanna@gmail.com>
2025-06-15 00:29:43 +0200

    jenkinsfile

--- test_register_user.py
+++ test_register_user.py
@@ -12,19 +12,19 @@ def test_register_user():
     service = ChromeService(executable_path="/opt/homebrew/bin/chromedriver")
     driver = webdriver.Chrome(service=service)
 
-    random_number = random.randint(1000, 9999)
-    unique_email = f"test{random_number}@test.pl"
+    # random_number = random.randint(1000, 9999)
+    # unique_email = f"test{random_number}@test.pl"
 
-    email = os.environ.get("EMAIL", unique_email)
-    password = os.environ.get("PASSWORD", "password123")
+    email = os.environ.get("EMAIL")
+    password = os.environ.get("TEST_PASSWORD")
 
     driver.get("http://seleniumdemo.com/")
     driver.find_element(By.XPATH, "//span[text()='My account']").click()
     driver.find_element(By.ID, "reg_email").send_keys(email)
     driver.find_element(By.ID, "reg_password").send_keys(password)
-
-    time.sleep(5)
     driver.find_element(By.NAME, "register").click()
+
+    # time.sleep(5)
 
     wait = WebDriverWait(driver, 10)
     entry_title = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "entry-title")))
